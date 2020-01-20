469375b3ec4ab2688282de624bf910aa592bd18b
Neil Patel Quinn <nquinn@wikimedia.org>
2020-01-20 21:59:17 +0000

    Update directory

--- update_publish_notebook.sh
+++ update_publish_notebook.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cd /home/neilpquinn-wmf
+cd /home/neilpquinn-wmf/proj
 { source .bash_profile
   date
   external-automatic-translation/fetch_edit_check_revert.py
   venv/bin/jupyter nbconvert --ExecutePreprocessor.timeout=600 --execute --to html external-automatic-translation/impact\ of\ external\ automatic\ translation\ services.ipynb
   cp external-automatic-translation/impact\ of\ external\ automatic\ translation\ services.html /srv/published-datasets/external-automatic-translation
-} >> /home/neilpquinn-wmf/external-automatic-translation/notebook_update.log 2>&1
+} >> /home/neilpquinn-wmf/proj/external-automatic-translation/notebook_update.log 2>&1
